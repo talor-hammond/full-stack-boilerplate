b435b59144358402b8a0f4f95c01e819a31c9b00
talor-hammond <talorjhammond@gmail.com>
2018-07-16 14:14:14 +1200

    serving to public; added webpack config

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+    render(
+            <App />,
+            document.getElementById('app')
+    )
+})
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const server = express()
 
+const path = require('path')
+
 // Routing
 const fruits = require('./routes/fruits')
 
 // Middleware
-server.use(express.json()) // parser
+server.use(express.json())
+server.use(express.static(path.join(__dirname, '../public'))) // serving our public/index.html
 
 server.use('/api/v1/fruits', fruits)
 // Routes
-server.get('/', (req, res) => {
-    res.send('Hi there')
-})
+
 
 module.exports = server
